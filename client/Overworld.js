974eb77282e1b8e2e2548fb149dcfd79e46ecb71
simon-kyger <simonkyger@hotmail.com>
2018-04-02 18:13:58 -0400

    map spans across entire window and locke move character tied to camera

--- client/Overworld.js
+++ client/Overworld.js
@@ -6,7 +6,7 @@ class Overworld extends Phaser.Scene {
     preload(){
         this.load.tilemapTiledJSON('map', 'data/zealotrymap.json');
         this.load.image('backgroundtiles', 'img/backgroundtiles.png');
-        this.load.image('players', 'img/players.gif');
+        this.load.spritesheet('players', 'img/players.gif', {frameWidth: 24, frameHeight: 32});
     }
 
     create(){
@@ -14,12 +14,22 @@ class Overworld extends Phaser.Scene {
         this.map = this.make.tilemap({key: 'map'});
         this.tileset = this.map.addTilesetImage('backgroundtiles');
         this.layer = this.map.createStaticLayer('Tile Layer 1', this.tileset, 0, 0);
-        this.layer.setScale(2);
         
         //player character data
-        const player = 'Locke';
-        this.players = this.add.image(400, 300, 'players');
-        this.players.setScale(2);
+        const config = {
+            key: 'move',
+            frames: this.anims.generateFrameNumbers('players', {
+                start: 6,
+                end: 7,
+            }),
+            frameRate: 10,
+            repeat: -1
+        };
+        this.anims.create(config);
+        this.player = this.add.sprite(300, 150, 'players').setScrollFactor(0);
+        this.player.fixedToCamera = true;
+        this.player.anims.play('move');
+        
         
         //camera
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -29,7 +39,7 @@ class Overworld extends Phaser.Scene {
             right: this.cursors.right,
             up: this.cursors.up,
             down: this.cursors.down,
-            speed: 1.0
+            speed: .25
         }
         this.controls = new Phaser.Cameras.Controls.Fixed(this.controlConfig);
     }
