9f55d0913666bc0cd6bd99f895b844f2073e9cd3
simon-kyger <simonkyger@hotmail.com>
2019-02-23 12:26:35 -0500

    ok fine change the port

--- server/configs/index.js
+++ server/configs/index.js
@@ -1,7 +1,7 @@
 const dbport = process.env.DBPORT || 27017;
 const config = {    
     hostname : 'localhost',
-    port : 80,    
+    port : process.env.PORT || 80,    
     dbport : dbport,
     dbname : process.env.DBNAME || `zealotrydb`,
     dburl : process.env.MONGODB_URI || `mongodb://localhost:${dbport}`
