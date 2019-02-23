622d4717a81a751aa1ca4063fa04eb7662de79de
simon-kyger <simonkyger@hotmail.com>
2019-02-23 11:30:49 -0500

    need heroku to build server so i can see what errors are occuring

--- serverbuild/models/character/validate.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.validateCharacter = validateCharacter;
-function validateCharacter(username) {
-    return true;
-}
