4f2555fea05715118abd27d9c0b14d181672b33d
simon-kyger <simonkyger@hotmail.com>
2019-02-23 09:58:58 -0500

    remove redundant variable

--- server/configs/local.js
+++ server/configs/local.js
@@ -1,9 +1,7 @@
-const dbport = process.env.DBPORT || 27017;
-
 export let localConfig = {    
     hostname : 'localhost',
     port : process.env.PORT || 80,    
-    dbport : dbport,
+    dbport : process.env.DBPORT || 27017,
     dbname : process.env.DBNAME || `zealotrydb`,
     dburl : process.env.MONGODB_URI || `mongodb://localhost:${dbport}`
 }
