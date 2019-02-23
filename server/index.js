49937db2f2ff64422031e7f7d4c6d6f7717af02c
simon-kyger <simonkyger@hotmail.com>
2019-02-23 07:46:00 -0500

    fix npm run dev and banner loading

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { config } from './configs';
 import * as Server from './main';
 
-fs.readFile('banner.txt', 'utf8', (err,data) => console.log(data));
+fs.readFile('./server/banner.txt', 'utf8', (err,data) => console.log(data));
 
 Server.create(config);
 Server.start();
