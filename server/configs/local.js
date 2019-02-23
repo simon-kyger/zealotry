6d67312a0ad560ff21771dd884ebd0534680a666
simon-kyger <simonkyger@hotmail.com>
2019-02-23 10:11:39 -0500

    oops :P

--- server/configs/local.js
+++ server/configs/local.js
@@ -1,7 +1,8 @@
+const dbport = process.env.DBPORT || 27017;
 export let localConfig = {    
     hostname : 'localhost',
     port : process.env.PORT || 80,    
-    dbport : process.env.DBPORT || 27017,
+    dbport : dbport,
     dbname : process.env.DBNAME || `zealotrydb`,
     dburl : process.env.MONGODB_URI || `mongodb://localhost:${dbport}`
 }
